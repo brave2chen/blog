8415bcd684e6b3c3358d8e791a2bcd92ee3cf6f3
陈庆勇 <chenqingyong@gdcattsoft.com>
2019-12-27 16:42:12 +0800

    init blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,75 @@
+module.exports = {
+  "title": "码到功成",
+  "description": "",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeLine/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/brave2chen",
+            "icon": "brave2chen-github"
+          },
+          {
+            "text": "简书",
+            "link": "https://www.jianshu.com/u/70093510485a",
+            "icon": "brave2chen-jianshu"
+          }
+        ]
+      }
+    ],
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "logo": "/head.gif",
+    "themePicker": false,
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "sidebar": "auto",
+    "lastUpdated": "Last Updated",
+    "author": "brave2chen",
+    "record": "备案编号",
+    "startYear": "2019"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
